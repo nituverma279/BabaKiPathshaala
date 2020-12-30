00132cb7334921cffcd1da798fb0924dbc69a2a2
Nitu Verma <nituverma279@gmail.com>
2020-12-30 19:49:23 +0530

    home page modification

--- geniusedu/static/assets/js/main.js
+++ geniusedu/static/assets/js/main.js
@@ -208,4 +208,21 @@
     aos_init();
   });
 
+  $("icofont-user-alt-7").on('click', function(){
+    if (!event.target.matches(newFunction())) {
+      var dropdowns = document.getElementsByClassName("dropdown-menu");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  });
+
+  function userFunction() {
+    document.getElementById("dropdown-menu").classList.toggle("hide");
+  }
+
 })(jQuery);
